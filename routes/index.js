f182b5c50431c47d8c540f50bd010f7623032224
Jason <jdudash@redhat.com>
2017-10-28 15:14:28 -0400

    adding skeleton

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'AWS S3 Image Viewer' });
+});
+
+module.exports = router;
