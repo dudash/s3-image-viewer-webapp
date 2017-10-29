1dbe365c4f5934c2b799b2026cf91169ad4e2567
Jason <jdudash@redhat.com>
2017-10-28 20:15:37 -0400

    basic patternfly layout working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AWS S3 Image Viewer' });
+  res.render('index', { title: 'AWS S3 Image Viewer', imageName: 'Dummy Image' });
 });
 
 module.exports = router;
