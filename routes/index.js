29a247264f5c4b5309b0c2f14902c11bf58e478d
Jason <jdudash@redhat.com>
2017-10-28 23:30:12 -0400

    error checking

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var bucketIds = ['Bucket1', 'Bucket2', 'AnotherBucket'];  // TODO pull these from ENV + user input
-  var pagedData = [''];  // TODO
+  var pagedData = [];  // TODO
   res.render('index', { title: 'AWS S3 Image Viewer', images: JSON.stringify(pagedData), buckets: JSON.stringify(bucketIds)});
 });
 
