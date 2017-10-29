629626f758975ab1dbda434858912a36ffdba46b
Jason <jdudash@redhat.com>
2017-10-29 00:53:08 -0400

    fixed the toasts

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var IMAGE_TYPEFILTER = process.env.IMAGE_TYPEFILTER || '.png,.jpg';
+
+/* validate the images and filter them according to prefs */
+function validateAndFilterImages(data) {
+    // validate URL syntax
+    // filter based on types listed (case insensitive)
+    return [];
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var bucketIds = ['Bucket1', 'Bucket2', 'AnotherBucket'];  // TODO pull these from ENV + user input
-  var pagedData = ['XXXX', 'YYYY', 'ZZZZ', 'AAAA', 'BBBB', 'CCCC', 'DDDD', 'EEEE', 'FFFF', 'GGGG'];
-  res.render('index', { title: 'AWS S3 Image Viewer', images: JSON.stringify(pagedData), buckets: JSON.stringify(bucketIds)});
+  var pagedData = ['https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
+                    'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
+                    'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png',
+                    'https://s3.amazonaws.com/rhdj2017-selfie-in/59bc8771815ba5440626d40d-59bc87725d92c77569d4ddc1.png'];
+  validatedPagedData = validateAndFilterImages(pagedData);
+  res.render('index', { title: 'AWS S3 Image Viewer', images: JSON.stringify(validatedPagedData), buckets: JSON.stringify(bucketIds)});
 });
 
 module.exports = router;
